19b9b9f70de04bb35d1a74f373933b857430b0dc
MathLeoYa <mateoleonardo1616@gmail.com>
2024-05-07 08:36:29 -0500

    a3
    a4

--- TareaPaquetes/src/Clases/Direccion.java
+++ TareaPaquetes/src/Clases/Direccion.java
@@ -3,5 +3,12 @@ package Clases;
 
 
 public class Direccion {
-    
+String algo;
+
+    public Direccion(String algo) {
+        this.algo = algo;
+    }
+    public Direccion() {
+    }
+    string algo2;
 }
