fcc19a45ec096384fcbe7386f52e5fc9a061425b
augusto20052008 <115821107+augusto20052008@users.noreply.github.com>
2024-05-15 22:17:28 -0500

    actualizaciones

--- Paqueteria/src/main/java/Logica/Repartidor.java
+++ Paqueteria/src/main/java/Logica/Repartidor.java
@@ -1,53 +1,17 @@
 package Logica;
-public class Repartidor extends Empleados{
+public class Repartidor extends Empleados {
+    private int idRepar;
     private int zona;
+    private 
 
     public Repartidor() {
     }
 
-    public Repartidor(int zona, String cuidad, String cedula, String apellido, String nombre, String email) {
-        super(cuidad, cedula, apellido, nombre, email);
+    public Repartidor(int idRepar, int zona, int idEmpl, String cuidad, String cedula, String apellido, String nombre, String email) {
+        super(idEmpl, cuidad, cedula, apellido, nombre, email);
+        this.idRepar = idRepar;
         this.zona = zona;
     }
-
-    public int getZona() {
-        return zona;
-    }
-
-    public void setZona(int zona) {
-        this.zona = zona;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
 
 }
